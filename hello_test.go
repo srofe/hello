dc440c518a0c23b1829b646d80d59b89bc2a9752
Simon Rofe <simon.rofe@ieee.org>
2022-01-04 15:09:15 +1000

    Extract assertion into a function to remove duplication

--- hello_test.go
+++ hello_test.go
@@ -3,19 +3,21 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	t.Run("The 'Hello' function shall return a message using the string passed.", func(t *testing.T) {
-		got := Hello("Simon")
-		want := "Hello, Simon"
+	assertCorrectMethod := func(t testing.TB, got, want string) {
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
 		}
+	}
+
+	t.Run("The 'Hello' function shall return a message using the string passed.", func(t *testing.T) {
+		got := Hello("Simon")
+		want := "Hello, Simon"
+		assertCorrectMethod(t, got, want)
 	})
 
 	t.Run("The 'Hello' function shall return a default message if empty string passed.", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, world"
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMethod(t, got, want)
 	})
 }
