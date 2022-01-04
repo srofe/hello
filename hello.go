4e78aeb3cacb2dc4dab4ee1e7e23474a0db19641
Simon Rofe <simon.rofe@ieee.org>
2022-01-04 15:26:10 +1000

    Extract prefix logic to a function

--- hello.go
+++ hello.go
@@ -12,15 +12,19 @@ func Hello(name string, language string) string {
 	if name == "" {
 		name = "world"
 	}
-	prefix := englishPrefix
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
 	switch language {
 	case french:
 		prefix = frenchPrefix
 	case spanish:
 		prefix = spanishPrefix
-
+	default:
+		prefix = englishPrefix
 	}
-	return prefix + name
+	return
 }
 
 func main() {
