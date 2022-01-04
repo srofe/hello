bfa1fb8d8687e6fa3f3c0e7bbe06a6234212c4df
Simon Rofe <simon.rofe@ieee.org>
2022-01-04 15:05:08 +1000

    Provide default name if none provided

--- hello_test.go
+++ hello_test.go
@@ -10,4 +10,12 @@ func TestHello(t *testing.T) {
 			t.Errorf("got %q want %q", got, want)
 		}
 	})
+
+	t.Run("The 'Hello' function shall return a default message if empty string passed.", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, world"
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 }
