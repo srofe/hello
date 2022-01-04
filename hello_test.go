aeb4e52defe40ca20aa36d52b0fd4f2f05aac73d
Simon Rofe <simon.rofe@ieee.org>
2022-01-04 15:01:40 +1000

    Use sub-test for the test case

--- hello_test.go
+++ hello_test.go
@@ -3,9 +3,11 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Simon")
-	want := "Hello, Simon"
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+	t.Run("The 'Hello' function shall return a message using the string passed.", func(t *testing.T) {
+		got := Hello("Simon")
+		want := "Hello, Simon"
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 }
